8bef6c581d65bed3884e6f95ffc8cdfcbf15d5e4
ksaniakhan <47515665+ksaniakhan@users.noreply.github.com>
2020-11-08 13:41:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(1200,800);
+
+  fixedRect=createSprite(200, 200, 50, 80);
+  movingRect=createSprite(400,200,80,50);
+
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+
+  fixedRect.debug=true;
+  movingRect.debug=true;
+}
+
+function draw() {
+  background(0);  
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  if(movingRect.x-fixedRect.x < movingRect.width/2+fixedRect.width/2  ){
+    fixedRect.shapeColor="red";
+    movingRect.shapeColor="red";
+  }
+  else{
+    fixedRect.shapeColor="green";
+    movingRect.shapeColor="green";
+  }
+
+
+  drawSprites();
+}
